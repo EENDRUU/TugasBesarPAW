1f58101a846006c4c0081d2f014148c779902bdf
lilykabelen <specialilyy@gmail.com>
2018-11-21 20:42:06 +0700

    g

---
+++ resources/js/components/home/dashboardMenuPelamar.vue
@@ -0,0 +1,150 @@
+<template>
+         <div> 
+                            <section class="hero is-info is-bold" >  
+                                <div class="hero-body hero-image" >   
+                                </div> 
+                                </section>
+                                <div class="isi">
+                                        <div class="columns">
+                                    
+                                            <div class="column logo">
+                                                    <aside class="menu">
+                                                        <p class="menu-label">
+                                                            DASHBOARD
+                                                        </p>
+                                                        <ul class="menu-list">
+                                                            
+                                                            <li><a>Cari Lowongan</a></li>
+                                                            <li><a>Edit Resume</a></li>
+                                                            <li><a>Profile Saya</a></li>
+                                                        </ul>
+                                            </div>
+                                    
+                                            <div class="column no-scroll">
+                                                    <div class="tile is-ancestor">
+                                                            <div class="tile is-vertical ">
+                                                                    <div class="tile is-parent">
+                                                                            <article class="tile is-child notification is-light">
+                                                                            <div class="content">
+                                                                                <p class="title">Buat Resume Online</p>
+                                                                                <p class="subtitle">Langkah 1</p>
+                                                                                <div class="content">
+                                                                                <!-- Content -->
+                                                                                </div>
+                                                                            </div>
+                                                                            </article>
+                                                                            
+                                                                        </div>
+                                                                    <div class="tile is-parent">
+                                                                        <article class="tile is-child notification is-light">
+                                                                                <div class="content">
+                                                                                <p class="title">Cari Lowongan Sesuai Bidang Anda</p>
+                                                                                <p class="subtitle">Langkah 2</p>
+                                                                                <div class="content">
+                                                                                    <!-- Content -->
+                                                                                </div>
+                                                                                </div>
+                                                                        </article>
+                                                                    </div>
+                                                                    <div class="tile is-parent">
+                                                                            <article class="tile is-child notification is-light">
+                                                                                <div class="content">
+                                                                                    <p class="title">Daftar Lowongan Yang Anda Pilih</p>
+                                                                                    <p class="subtitle">Langkah 3</p>
+                                                                                    <div class="content">
+                                                                                    <!-- Content -->
+                                                                                    </div>
+                                                                                </div>
+                                                                        </article>
+                                                                    </div>
+                                                            
+                                                            </div>
+                                                        
+                                                        </div>
+                                            </div>
+                                    
+                                        </div>
+                                    </div>
+                            </div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+            datas:[]
+        }
+    },
+    created(){
+        this.getData();
+    },
+
+    methods:{
+        getData(){
+             let uri="/api/Lowongan";
+             axios.get(uri).then((response) => {
+                 console.log(response);
+                 this.datas = response.data;
+            }).catch(error => {
+                this.$toast.open({
+                    duration: 2000,
+                    message: error,
+                    position: 'is-bottom',
+                    type: 'is-danger',
+                    queue: false,
+                })
+            });
+        }
+    }
+}
+</script>
+
+<style>
+            .panel{
+                max-width: 25%
+                
+                
+            }
+            html {
+                    height: 100vh;
+                    width: 100vw;
+                }
+            
+                body {
+                    height: 100%;
+                    width: 100%;
+                }
+            
+                .columns {
+                    height: 100%;
+                }
+            
+                .column.no-scroll {
+                    overflow: auto;
+                }
+            
+                .column.logo {
+                    max-width: 20%;
+                    margin-left: 2%;
+                    display: flex;
+                    justify-content: left;
+                    align-items: left;
+                }
+            
+                .placeholder {
+                    background-color: cyan;
+                    margin: 2rem;
+                    height: 250px;
+                }
+                .hero-image{
+                    background-image: url("img/tileatasPelamar.jpg");
+                    background-position: center center;
+                    background-repeat: no-repeat;
+                    background-attachment: fixed;
+                    background-size: auto;
+                    background-color: #999;
+                }
+                .isi{
+                margin-top: 2%
+                }
+            
+ </style>
